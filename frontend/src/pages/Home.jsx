10d919f66bb75ba8d3031c0b43e9f05d65994751
edwinkim16 <edwinkimathi226@gmail.com>
2025-10-27 16:44:34 +0300

    note form

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -44,8 +44,38 @@ function Home(){
             .catch((err) => alert(err));
     };
 
-    return (<>
-    <div>Home</div>
-    </>) 
+    return ( 
+    <div>
+            <div>
+                <h2>Notes</h2>
+                {notes.map((note) => (
+                    <Note note={note} onDelete={deleteNote} key={note.id} />
+                ))}
+            </div>
+            <h2>Create a Note</h2>
+            <form onSubmit={createNote}>
+                <label htmlFor="title">Title:</label>
+                <br />
+                <input
+                    type="text"
+                    id="title"
+                    name="title"
+                    required
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                />
+                <label htmlFor="content">Content:</label>
+                <br />
+                <textarea
+                    id="content"
+                    name="content"
+                    required
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                ></textarea>
+                <br />
+                <input type="submit" value="Submit"></input>
+            </form>
+    </div>) 
 }
 export default Home;
